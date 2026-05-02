dc56a3c52abe5ed56fe564fedebfd950bad92bc2
k87903524-creator <k87903524@gmail.com>
2026-05-02 21:42:46 +0900

    프로그램 시작 및 종료

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(" == 프로그램 시작 ==");
+    Scanner sc = new Scanner(System.in);
+
+    while (true) {
+      System.out.print("명령어) ");
+      String cmd = sc.nextLine().trim();
+
+      if (cmd.equals("exit")) {
+        break;
+      } else if (cmd.length() == 0) {
+        System.out.println("명령어를 입력해주세요");
+        continue;
+      }
+    }
+    System.out.println(" == 프로그램 종료 ==");
+    sc.close();
+  }
+}
