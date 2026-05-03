2d9ba4431f9fb4d53d51728f408662e34566cf8a
k87903524-creator <k87903524@gmail.com>
2026-05-03 21:05:15 +0900

    게시글 리스트 완료

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,8 @@ public class Main {
         System.out.println("명령어를 입력해주세요");
         continue;
       } else if (cmd.equals("article write")) {
-
-        int id = lastArticle +1;
+        System.out.println("== 게시글 작성 ==");
+        int id = lastArticle + 1;
 
         System.out.print("제목 : ");
         String title = sc.nextLine().trim();
@@ -31,10 +31,20 @@ public class Main {
 
         Article article = new Article(id, title, body);
         articles.add(article);
-        System.out.println(id +"번 글 작성 완료");
+        System.out.println(id + "번 글 작성 완료");
 
         lastArticle++;
-
+      } else if (cmd.equals("article list")) {
+        System.out.println("==게시글 목록==");
+        if (articles.size() == 0) {
+          System.out.println("게시글이 존재하지 않습니다.");
+        } else {
+          System.out.println("  번호  /  제목  /  내용  ");
+          for (int i = articles.size() - 1; i >= 0; i--) {
+            Article article = articles.get(i);
+            System.out.printf("  %d  /  %s  /  %s  \n", article.getId(), article.getTitle(), article.getBody());
+          }
+        }
       }
     }
     System.out.println(" == 프로그램 종료 ==");
@@ -42,11 +52,12 @@ public class Main {
   }
 }
 
-class Article{
+class Article {
   private int id;
   private String title;
   private String body;
-  public Article (int id, String title, String body){
+
+  public Article(int id, String title, String body) {
     this.id = id;
     this.title = title;
     this.body = body;
