be04321175f40961c96f88c05fe08de2ae90004d
k87903524-creator <k87903524@gmail.com>
2026-05-10 18:59:41 +0900

    게시글 수정, 작성일시 추가

--- src/Main.java
+++ src/Main.java
@@ -43,7 +43,7 @@ public class Main {
         String body = sc.nextLine().trim();
 
         // 객체 생성
-        Article article = new Article(id, title, body);
+        Article article = new Article(id,Util.getNowStr(), Util.getNowStr(), title, body);
         // 리스트에 저장
         articles.add(article);
         System.out.println(id + "번 글 작성 완료");
@@ -83,11 +83,13 @@ public class Main {
           continue;
         }
         System.out.println("번호 : " + foundArticle.getId());
+        System.out.println("작성일시 : " + foundArticle.getRegDate());
+        System.out.println("수정일시 : " + foundArticle.getUpdateDate());
         System.out.println("제목 : " + foundArticle.getTitle());
         System.out.println("내용 : " + foundArticle.getBody());
       }
       // 게시글 삭제
-      else if (cmd.startsWith("article delete")) {
+      else if (cmd.startsWith("article delete ")) {
         int id = Integer.parseInt(cmd.split(" ")[2]);
 
         Article foundArticle = null;
@@ -102,49 +104,43 @@ public class Main {
           System.out.println("해당 게시글은 없습니다.");
           continue;
         }
-        // 해당 게시글 삭제
+        // 게시글 삭제
         articles.remove(foundArticle);
         System.out.println(id + "번 글이 삭제되었습니다.");
+      }
+      // 게시글 수정
+      else if (cmd.startsWith("article modify ")) {
+        int id = Integer.parseInt(cmd.split(" ")[2]);
+
+        Article foundArticle = null;
+
+        for (Article article : articles) {
+          if (article.getId() == id) {
+            foundArticle = article;
+            break;
+          }
+        }
+        if (foundArticle == null) {
+          System.out.println("해당 게시글은 없습니다.");
+        }
+
+        System.out.println("기존 제목 : " + foundArticle.getTitle());
+        System.out.println("기존 내용 : " + foundArticle.getBody());
+
+        //수정할 데이터 생성
+        System.out.print("수정할 제목 : ");
+        String newTitle = sc.nextLine().trim();
+        System.out.print("수정할 내용 : ");
+        String newBody = sc.nextLine().trim();
+
+        //수정 데이터 저장
+        foundArticle.setTitle(newTitle);
+        foundArticle.setBody(newBody);
+        foundArticle.setUpdateDate(Util.getNowStr());
+        System.out.println(id + "번 게시글이 수정되었습니다.");
       }
     }
     System.out.println(" == 프로그램 종료 ==");
     sc.close();
-  }
-}
-
-// 게시글 저장소, 생성자, 접근제한자, getter/setter 적용
-class Article {
-  private int id;
-  private String title;
-  private String body;
-
-  public Article(int id, String title, String body) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getBody() {
-    return body;
-  }
-
-  public void setBody(String body) {
-    this.body = body;
   }
 }
